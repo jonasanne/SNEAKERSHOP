d40f9be0e57fef55c910d2d186c4690dda23e64a
unknown <jonas.anne@outlook.com>
2021-01-08 18:16:10 +0100

    cart page responsive

--- sneakershop/src/App.vue
+++ sneakershop/src/App.vue
@@ -11,49 +11,45 @@ import Navigation from "./presentations/shared/components/Navigation.vue";
 
 export default defineComponent({
   components: {
-    Navigation
-  }
+    Navigation,
+  },
 });
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;600;700&display=swap');
-#app{
-
-  font-family: 'Titillium Web', sans-serif;
+@import url("https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;600;700&display=swap");
+#app {
+  font-family: "Titillium Web", sans-serif;
 }
 
 $colors-neutral: (
-    "light-grey": #FBFBFB,
-    "dark-grey" : #F6F6F6,
-    "mint" : #26f8e2
+  "light-grey": #fbfbfb,
+  "dark-grey": #f6f6f6,
+  "mint": #26f8e2,
 );
 
-
 html {
-    transition: background 1500ms ease-out;
-    background: var(--global-background-color);
-    font-family: 'Titillium Web', sans-serif;
+  transition: background 1500ms ease-out;
+  background: var(--global-background-color);
+  font-family: "Titillium Web", sans-serif;
 }
 
-
 // buttons
-.c-app__button{
+.c-app__button {
   all: unset;
   width: 100%;
   text-decoration: none;
   max-width: 300px;
   margin: auto;
   cursor: pointer;
-  &:hover >.c-app__button-underline::before,
-  &:hover > .c-app__button-underline::after{
-  width: 50%;
-
+  &:hover > .c-app__button-underline::before,
+  &:hover > .c-app__button-underline::after {
+    width: 50%;
   }
 }
 //span with this class for underline animation
-.c-app__button-underline{
-  margin-top: 2px;
+.c-app__button-underline {
+  margin-top: 1px;
   display: block;
   position: relative;
   width: 100%;
@@ -61,74 +57,86 @@ html {
   margin: auto;
   &::before,
   &::after {
-      content: "";
-      height: 2px;
-      width: 0;
-      bottom: 1px;
-      position: absolute;
-      background: black;
-      transition: 0.2s ease all;
-      -moz-transition: 0.2s ease all;
-      -webkit-transition: 0.2s ease all;
+    content: "";
+    height: 1px;
+    width: 0;
+    bottom: 1px;
+    position: absolute;
+    background: black;
+    transition: 0.2s ease all;
+    -moz-transition: 0.2s ease all;
+    -webkit-transition: 0.2s ease all;
   }
   &::before {
-      left: 50%;
+    left: 50%;
   }
   &::after {
-      right: 50%;
+    right: 50%;
   }
 }
 //UTILS
-.z-m-1{
+.z-m-1 {
   z-index: -1;
 }
-.all-unset{
+.all-unset {
   all: unset;
 }
-
 
 //-----------COMPONENTS-----------
 
 //BRANDS
-.wrapper-card{
+.wrapper-card {
   flex: 0 0 auto;
   max-width: 150px;
 }
 //CART
-.c-app-cart-summary{
+.c-app-cart-summary {
   background-color: map-get($map: $colors-neutral, $key: "dark-grey");
 }
-.c-app-cartitem{
+.c-app-cartitem {
   max-height: 140px;
-  
 }
-.c-app-cartitem__image-container{
+.c-app-cartitem__image-container {
   max-width: 140px;
   max-height: 140px;
 }
-.c-app-cartitem__button{
+.c-app-cartitem__button {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 40px;
-  height: 40px;
-}
-.c-app-cartitem__item{
-  max-width: 300px;
-  width: 100%;
+  width: 20px;
+  height: 20px;
+  
 }
 
-
-
+.c-app-cartitem__item {
+  max-width: 200px;
+}
+.svg-button{
+  width: 12px;
+  height: 12px;
+}
+.svg-button-cross{
+  width: 10px;
+  height: 10px;
+  fill: black;
+}
 //UNDERLINE
-.underline{
+.underline {
   border-top: white 2px solid;
   max-width: 80%;
   margin-top: 0.3rem;
 }
 
-
-
-
-
+$breakpoint-medium: 768px;
+@media (min-width: $breakpoint-medium) {
+  .c-app-cartitem__item {
+    max-width: 300px;
+    width: 100%;
+  }
+  .c-app-cartitem__button {
+    width: 40px;
+    height: 40px;
+  }
+}
 </style>
--- sneakershop/src/presentations/shoes/views/Cart.vue
+++ sneakershop/src/presentations/shoes/views/Cart.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col md:flex-row justify-between ">
-    <div class="pt-8 w-3/4 pl-20">
-      <!-- left -->
+  <div class="flex flex-col md:flex-row justify-between  ">
+    <!-- left -->
+    <div class="pt-8 md:w-3/4 md:pl-14 px-6 ">
       <h1 class=" font-semibold text-3xl">My Cart</h1>
       <div class="h-full flex-col flex">
         <!-- cart items -->
@@ -18,132 +18,116 @@
             />
           </div>
           <!-- name shoes -->
-          <div class=" ml-8">
-            <p class="text-xl">
-              Nike <span class="font-semibold">Court</span> Vision Low
-            </p>
-            <p class="font-semibold text-lg">Black</p>
-          </div>
-          <!-- amount shoes -->
-          <div class="flex justify-center items-center c-app-cartitem__item">
-            <button
-              class="rounded-full bg-darkGrey c-app-cartitem__button mr-3"
+          <div
+            class="ml-2 md:ml-8 flex md:flex-row flex-col  justify-between w-full"
+          >
+            <div>
+              <p class=" text-sm md:text-xl">
+                Nike <span class="font-semibold">Court</span> Vision Low
+              </p>
+              <p class="font-semibold md:text-lg">Black</p>
+            </div>
+            <!-- amount shoes -->
+            <div
+              class="flex  c-app-cartitem__item md:justify-center md:items-center"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20.0"
-                height="5.69"
-                viewBox="0 0 26.551 5.69"
+              <button
+                class="rounded-full bg-darkGrey c-app-cartitem__button mr-3"
               >
-                <path
-                  id="Icon_awesome-minus"
-                  data-name="Icon awesome-minus"
-                  d="M24.655,2.031H1.9A1.9,1.9,0,0,0,0,3.928v1.9a1.9,1.9,0,0,0,1.9,1.9H24.655a1.9,1.9,0,0,0,1.9-1.9v-1.9A1.9,1.9,0,0,0,24.655,2.031Z"
-                  transform="translate(0 -2.031)"
-                  fill="#dcdcdc"
-                />
-              </svg>
-            </button>
-            <!-- amount count -->
-            <p class="text-2xl">1</p>
-            <button
-              class="rounded-full bg-darkGrey c-app-cartitem__button ml-3"
-            >
-              <a href="">
                 <svg
+                  class="svg-button"
                   xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="26.441"
-                  viewBox="0 0 26.441 26.441"
+                  viewBox="0 0 26 6"
                 >
-                  <path
-                    id="Union_1"
-                    data-name="Union 1"
-                    d="M12.277,26.441a1.888,1.888,0,0,1-1.889-1.888v-8.5h-8.5A1.887,1.887,0,0,1,0,14.165V12.276a1.888,1.888,0,0,1,1.886-1.889h8.5v-8.5A1.888,1.888,0,0,1,12.277,0h1.888a1.888,1.888,0,0,1,1.889,1.888v8.5h8.5a1.888,1.888,0,0,1,1.889,1.889v1.889a1.888,1.888,0,0,1-1.889,1.889h-8.5v8.5a1.888,1.888,0,0,1-1.889,1.888Z"
+                  <rect
+                    id="Rectangle_15"
+                    data-name="Rectangle 15"
+                    width="26"
+                    height="6"
+                    rx="3"
+                    fill="#dcdcdc"
                   />
                 </svg>
-              </a>
-            </button>
-          </div>
-          <!-- price shoes -->
-          <div class=" flex justify-center items-center c-app-cartitem__item">
-            <p class="text-xl">€145</p>
-          </div>
-          <!-- trash shoes -->
-          <div class="flex justify-center items-center c-app-cartitem__item ">
-            <button
-              class="rounded-full bg-darkGrey c-app-cartitem__button mr-3"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20.955"
-                height="23.172"
-                viewBox="0 0 20.955 23.172"
+              </button>
+              <!-- amount count -->
+              <p class="md:text-2xl ">1</p>
+              <button
+                class="rounded-full bg-darkGrey c-app-cartitem__button ml-3"
               >
-                <g
-                  id="Icon_feather-trash"
-                  data-name="Icon feather-trash"
-                  transform="translate(0.5 0.5)"
+                <svg
+                  class="svg-button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 25.999 26"
                 >
                   <path
-                    id="Path_8"
-                    data-name="Path 8"
-                    d="M1.5,3H21.455"
-                    transform="translate(-1.5 1.434)"
-                    fill="none"
-                    stroke="#000"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1"
+                    id="Union_3"
+                    data-name="Union 3"
+                    d="M-2461-1261.858v-7h-7a3,3,0,0,1-3-3,3,3,0,0,1,3-3h7v-7a3,3,0,0,1,3-3,3,3,0,0,1,3,3v7h7a3,3,0,0,1,3,3,3,3,0,0,1-3,3h-7v7a3,3,0,0,1-3,3A3,3,0,0,1-2461-1261.858Z"
+                    transform="translate(2471 1284.858)"
                   />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div
+            class=" flex justify-between items-end c-app-cartitem__item flex-col-reverse md:flex-row md:justify-around md:items-center"
+          >
+            <!-- price shoes -->
+            <p class="md:text-xl">€145</p>
+            <!-- trash shoes -->
+            <div class="flex justify-end items-center c-app-cartitem__item w-max">
+              <button class="rounded-full bg-darkGrey c-app-cartitem__button ">
+                <svg
+                class="svg-button-cross"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20.142 20.142"
+                >
                   <path
-                    id="Path_9"
-                    data-name="Path 9"
-                    d="M18.02,5.434v15.52A2.217,2.217,0,0,1,15.8,23.172H4.717A2.217,2.217,0,0,1,2.5,20.955V5.434m3.326,0V3.217A2.217,2.217,0,0,1,8.043,1h4.434a2.217,2.217,0,0,1,2.217,2.217V5.434"
-                    transform="translate(-0.283 -1)"
-                    fill="none"
-                    stroke="#000"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1"
+                    id="Union_2"
+                    data-name="Union 2"
+                    d="M-2744.767-1264.089l-4.95-4.95-4.949,4.95a3,3,0,0,1-4.243,0,3,3,0,0,1,0-4.243l4.95-4.95-4.95-4.95a3,3,0,0,1,0-4.243,3,3,0,0,1,4.243,0l4.949,4.95,4.95-4.95a3,3,0,0,1,4.242,0,3,3,0,0,1,0,4.243l-4.95,4.95,4.95,4.95a3,3,0,0,1,0,4.243,2.992,2.992,0,0,1-2.121.878A2.991,2.991,0,0,1-2744.767-1264.089Z"
+                    transform="translate(2759.788 1283.353)"
                   />
-                </g>
-              </svg>
-            </button>
-            <!-- delete -->
+                </svg>
+              </button>
+              <!-- delete -->
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="bg-darkGrey pt-8 w-1/4  h-screen">
+
+    <!-- right -->
+    <div class="bg-darkGrey pt-8 md:w-1/4  md:h-screen mt-12  md:mt-0">
       <!-- right -->
       <div>
         <!-- top -->
-        <h1 class="font-semibold text-3xl px-10">Summary</h1>
+        <h1 class="font-semibold text-3xl md:px-10 px-6">Summary</h1>
         <hr class="underline" />
-        <div class="flex justify-between mb-2 mt-6 px-10">
+        <div class="flex justify-between  mt-6 md:px-10 px-6">
           <!-- subtotal -->
-          <p class="text-xl">Subtotal</p>
-          <p class="text-xl font-semibold">€290</p>
+          <p class="md:text-xl">Subtotal</p>
+          <p class="md:text-xl font-semibold">€290</p>
         </div>
-        <div class="flex justify-between px-10">
+        <div class="flex justify-between md:px-10 px-6">
           <!-- shipping -->
-          <p class="text-xl font">Shipping</p>
-          <p class="text-xl font-semibold">€7.99</p>
+          <p class="md:text-xl font">Shipping</p>
+          <p class="md:text-xl font-semibold">€7.99</p>
         </div>
       </div>
-      <div class="bottom mt-80 px-10 flex align-middle flex-col">
+      <div class="bottom md:mt-80 px-6 md:px-10 mt-4  flex align-middle flex-col">
         <div class="flex justify-between ">
           <!-- total -->
-          <p class="text-3xl">Total</p>
-          <p class="text-3xl font-semibold">€297.99</p>
+          <p class="md:text-3xl">Total</p>
+          <p class="md:text-3xl font-semibold">€297.99</p>
         </div>
         <!-- buton -->
-        <div class="uppercase c-app__button py-4 text-center bg-mint mt-10">
+        <div class="uppercase c-app__button py-4 text-center bg-mint mt-10 mb-10">
           <p class="">
             checkout(
-            <span class="font-bold ">€152.99</span>
+            <span class="font-bold">€152.99</span>
             )
           </p>
           <span class="c-app__button-underline"></span>
